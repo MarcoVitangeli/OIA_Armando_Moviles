c3b4dea2fe64e74ccf5ef7138c08d32d8aeaf068
Mastro <diegomastro99@gmail.com>
2021-09-17 20:36:50 -0300

    merge tests

--- test/solucion/CantMovilesTest.java
+++ test/solucion/CantMovilesTest.java
@@ -9,36 +9,64 @@ import org.junit.Test;
 
 public class CantMovilesTest {
 
-	
 	@Test
 	public void enunciado() {
 		List<Colgante> colgantes = new ArrayList<>();
-		
-		colgantes.add(new Colgante(4));
-		colgantes.add(new Colgante(3));
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(2));
-		colgantes.add(new Colgante(3));
-		colgantes.add(new Colgante(1));
-		
+		int[] pesos = { 4, 3, 1, 2, 3, 1 };
+
+		for (int peso : pesos) {
+			colgantes.add(new Colgante(peso));
+		}
+
 		Movil movil = new Movil(colgantes);
+
 		assertEquals("8 2", movil.cantMoviles());
 	}
-		
+
 	@Test
-	public void impares() {
+	public void todosIguales() {
 		List<Colgante> colgantes = new ArrayList<>();
-		
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(1));
-		colgantes.add(new Colgante(1));
-		
+		int[] pesos = { 4, 4, 4, 4 };
+
+		for (int peso : pesos) {
+			colgantes.add(new Colgante(peso));
+		}
+
 		Movil movil = new Movil(colgantes);
-		assertEquals("6 3", movil.cantMoviles());
+		assertEquals("16 2", movil.cantMoviles());
+	}
+
+	@Test
+	public void todosDistintosCantidadMaxima() {
+		List<Colgante> colgantes = new ArrayList<>();
+
+		for (int i = 1; i <= 100; i++) {
+			colgantes.add(new Colgante(i));
+		}
+
+		Movil movil = new Movil(colgantes);
+		assertEquals("no se puede", movil.cantMoviles());
+	}
+
+	@Test
+	public void sinColgantes() {
+		List<Colgante> colgantes = new ArrayList<>();
+
+		Movil movil = new Movil(colgantes);
+
+		assertEquals("no se puede", movil.cantMoviles());
+	}
+
+	@Test
+	public void maximoValor() {
+		List<Colgante> colgantes = new ArrayList<>();
+
+		for (int i = 1; i <= 1000; i++) {
+			colgantes.add(new Colgante(100));
+		}
+
+		Movil movil = new Movil(colgantes);
+		assertEquals("100000 500", movil.cantMoviles());
 	}
 
 }
